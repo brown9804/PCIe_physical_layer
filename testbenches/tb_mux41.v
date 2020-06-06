7b9272172090d3aad36f2d9b9fe9ecad4d10d87b
Belinda Brown <belindabrownr04@gmail.com>
2020-06-06 03:46:09 +0000

    Update tb_mux41.v

--- testbenches/tb_mux41.v
+++ testbenches/tb_mux41.v
@@ -1,52 +1,52 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
+// Company: 
+// Engineer: 
+// 
 // Create Date: 22.04.2020 00:56:45
-// Design Name:
+// Design Name: 
 // Module Name: bancoPruebas
-// Project Name:
-// Target Devices:
-// Tool Versions:
-// Description:
-//
-// Dependencies:
-//
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-//
+// 
 //////////////////////////////////////////////////////////////////////////////////
-`include "./src/mux4x1_behav.v"
-`include "./syn/mux4x1_syn.v"
-`include "./testers/t_mux41.v"
-`include "./lib/cmos_cells.v"
+`include "mux4x1_behav.v"
+`include "mux4x1_struct.v"
+`include "tester.v"
+`include "cmos_cells.v"
 
 module bancoPruebas();
 
 
-wire [7:0] in0;
+wire [7:0] in0;	
 wire [7:0] in1;
 wire [7:0] in2;
 wire [7:0] in3;
-wire reset;
+wire reset;	
 wire clk1f;			// slowest
 wire clk2f;
-wire clk4f;
+wire clk4f;	
 //wire clk32f;		// faster
 wire clk;			// root clock
-wire [3:0] valid;
-wire [7:0] outc;
+wire [3:0] valid;					
+wire [7:0] out;
 wire [7:0] outs;
 
 
 // conexiones e instanciado
 
-mux4x1_behav	 	mux_behav(
-						.out(outc),
+mux4x1_behav	 	mux_behav(		                       
+						.out(out),
 						.in0(in0),
-                       				.in1(in1),
+                        .in1(in1),
 						.in2(in2),
 						.in3(in3),
 						.valid(valid),
@@ -54,13 +54,13 @@ mux4x1_behav	 	mux_behav(
 						.clk4f(clk4f),
 						.clk2f(clk2f),
 						.clk1f(clk1f)
-
+						
 );
 
-mux4x1_struct	 	mux_struct(
+mux4x1_struct	 	mux_struct(		                       
 						.out(outs),
 						.in0(in0),
-                        			.in1(in1),
+                        .in1(in1),
 						.in2(in2),
 						.in3(in3),
 						.valid(valid),
@@ -68,16 +68,16 @@ mux4x1_struct	 	mux_struct(
 						.clk4f(clk4f),
 						.clk2f(clk2f),
 						.clk1f(clk1f)
-
+						
 );
 
 
 
 
-tester Tb(       				.outc(outc),
+tester Tb(       		.outc(out),
 						.outs(outs),
 						.in0(in0),
-                        			.in1(in1),
+                        .in1(in1),
 						.in2(in2),
 						.in3(in3),
 						.valid(valid),
